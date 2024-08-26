d3eb9b628819d35cd4f0bf172cbf3aaae702210d
Zanuda <zanuda.brat.zarazy@gmail.com>
2024-08-26 10:08:48 +0300

    Fixed offsetWidth and offsetLeft values bug

--- src/scripts/cleaning-slider.js
+++ src/scripts/cleaning-slider.js
@@ -15,33 +15,34 @@ document.querySelectorAll(".cleaning__slider-sidebar-btn").forEach((el) => {
     // Change active button
     activeBtn.classList.remove("active");
     activeBtn = el;
-    activeBtn.classList.add("active")
+    activeBtn.classList.add("active");
 
     sliderSidebarEl.classList.contains("active") && sliderSidebarEl.classList.remove("active"); // To hide panel on mobile
   });
 });
 
-// Slider default view preparations
-const contentWidthAndPosExampleEl = document.getElementById("content-width-and-pos-example");
-const sliderImageBlock = sliderEl.querySelector("#cleaning__slider-image-block");
+window.addEventListener("load",  () => {
+  // Slider default view preparations
+  const contentWidthAndPosExampleEl = document.getElementById("content-width-and-pos-example");
+  const sliderImageBlock = sliderEl.querySelector("#cleaning__slider-image-block");
 
-positionSliderPreview();
+  positionSliderPreview();
 
-window.addEventListener("resize", positionSliderPreview);
+  window.addEventListener("resize", positionSliderPreview);
 
-function positionSliderPreview() {
-  sliderImageBlock.style.transform = `translate(${contentWidthAndPosExampleEl.offsetLeft - sliderImageBlock.offsetLeft}px, -50px) scale(${contentWidthAndPosExampleEl.offsetWidth / sliderImageBlock.offsetWidth})`
-}
-
-// On scrolling logic
-document.addEventListener("scroll", handleDocumentScrollForCleaningSlider);
-
-function handleDocumentScrollForCleaningSlider() {
-  if (window.scrollY + document.documentElement.clientHeight > sliderEl.offsetTop + sliderEl.offsetHeight / 2) {
-    sliderEl.classList.add("active");
-  } else {
-    sliderEl.classList.remove("active");
+  function positionSliderPreview() {
+    sliderImageBlock.style.transform = `translate(${contentWidthAndPosExampleEl.offsetLeft - sliderImageBlock.offsetLeft}px, -50px) scale(${contentWidthAndPosExampleEl.offsetWidth / sliderImageBlock.offsetWidth})`;
   }
-}
-// For page refresh
-handleDocumentScrollForCleaningSlider();
+  // On scrolling logic
+  document.addEventListener("scroll", handleDocumentScrollForCleaningSlider);
+
+  function handleDocumentScrollForCleaningSlider() {
+    if (window.scrollY + document.documentElement.clientHeight > sliderEl.offsetTop + sliderEl.offsetHeight / 2) {
+      sliderEl.classList.add("active");
+    } else {
+      sliderEl.classList.remove("active");
+    }
+  }
+  // For page refresh
+  handleDocumentScrollForCleaningSlider();
+});
